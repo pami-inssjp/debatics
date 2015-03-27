36a21df4954123eb63a5cbdb01b825af7488a77f
pablodgonzalez <pablodgonzalez@gmail.com>
2015-03-27 19:12:49 -0300

    check root user

--- run_debatics.sh
+++ run_debatics.sh
@@ -1,3 +1,6 @@
+if [ "$(id -u)" != "0" ];
+	then echo "This script must be run as root with -E parameter for proxy settings" 1>&2 exit 1
+fi
 
 systemctl start docker
 docker run -d -p 27017:27017 --name mongo mongo
@@ -15,6 +18,8 @@ docker run -d -p 80:80 \
 -e "http_proxy=$http_proxy" \
 --link mongo:mongo \
 --name debatics democracyos/app:0.14.0-deis
-docker exec -d debatics git config --global http.proxy $http_proxy
-docker exec -d debatics git config --global https.proxy $http_proxy
+docker exec -d debatics git config --system http.proxy $http_proxy
+docker exec -d debatics git config --system https.proxy $http_proxy
 docker exec -d debatics git clone https://github.com/pami-inssjp/debatics.git
+docker exec -d debatics cp -fv debatics/logo_debatics.png public/lib/boot/images/logo_debatics.png
+docker exec -d debatics cp -fv debatics/app.css public/app.css
