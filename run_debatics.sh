09bf924267c381766bde9a1534491dbc91484b90
pablodgonzalez <pablodgonzalez@gmail.com>
2015-03-27 19:17:11 -0300

    check root user

--- run_debatics.sh
+++ run_debatics.sh
@@ -1,5 +1,6 @@
 if [ "$(id -u)" != "0" ];
-	then echo "This script must be run as root with -E parameter for proxy settings" 1>&2 exit 1
+	then echo "This script must be run as root with -E parameter for proxy settings" 1>&2
+	exit 1
 fi
 
 systemctl start docker
